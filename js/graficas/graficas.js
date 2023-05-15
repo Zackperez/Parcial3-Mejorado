2b41dcac0ae248cf5b2b5cf5e3685e2934b7fb9b
Zacks <53922473+Zackperez@users.noreply.github.com>
2023-05-15 17:33:42 -0500

    Todas las paginas (a excepcion de graficas.html) responsive

--- js/graficas/graficas.js
+++ js/graficas/graficas.js
@@ -4,11 +4,19 @@ const Controlador = {
     mostrarRegistrosTablas: function () {
         axios({
             method: 'GET',
-            url: 'https://mmphzayxvvhdtrtcvjsq.supabase.co/rest/v1/registros_prueba?select=*',
+            url: 'https://mmphzayxvvhdtrtcvjsq.supabase.co/rest/v1/datos_csv?select=*',
             headers: config.headers
         })
             .then(function (response) {
-                Vista.mostrarRegistrosTablas(response.data);
+
+                response.data.forEach(element => {
+
+
+                    console.log(element)
+
+                });
+
+                //Vista.mostrarRegistrosTablas(response.data);
             })
             .catch(function (error) {
                 console.log(error)
@@ -50,61 +58,12 @@ const Controlador = {
 const Vista = {
     /* PAGINA PRINCIPAL */
     mostrarRegistrosTablas: function (data) {
-        // Paso 2: Agrupar los datos por valor de la columna
-        const groupedData = data.reduce((accumulator, item) => {
-            const columnValue = item.irradiat; // Reemplaza 'nombre_de_columna' con el nombre real de la columna
-
-            if (accumulator.hasOwnProperty(columnValue)) {
-                accumulator[columnValue]++;
-            } else {
-                accumulator[columnValue] = 1;
-            }
-
-            return accumulator;
-        }, {});
-
-        // Paso 3: Preparar los datos para la gráfica
-        const labels = Object.keys(groupedData);
-        const values = Object.values(groupedData);
-
-        // Paso 4: Configurar y renderizar la gráfica
-        const canvas = document.getElementById('myChart');
-        const chart = new Chart(canvas, {
-            type: 'bar',
-            data: {
-                labels: labels,
-                datasets: [{
-                    label: 'Datos',
-                    data: values,
-                    backgroundColor: 'rgba(75, 192, 192, 0.8)',
-                }]
-            },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
-                }
-            }
-        });
-
-        // Renderiza la gráfica
-        chart.render();
+        console.log(data)
     },
 
     mostrarRegistrosTablas2: function (data) {
         // Paso 2: Agrupar los datos por valor de la columna
-        const groupedData = data.reduce((accumulator, item) => {
-            const columnValue = item.deg_malig; // Reemplaza 'nombre_de_columna' con el nombre real de la columna
 
-            if (accumulator.hasOwnProperty(columnValue)) {
-                accumulator[columnValue]++;
-            } else {
-                accumulator[columnValue] = 1;
-            }
-
-            return accumulator;
-        }, {});
 
         // Paso 3: Preparar los datos para la gráfica
         const labels = Object.keys(groupedData);
@@ -188,7 +147,5 @@ document.addEventListener('DOMContentLoaded', function () {
     Controlador.mostrarRegistrosTablas();
     Controlador.mostrarRegistrosTablas2();
     Controlador.mostrarRegistrosTablas3();
-
-
 })
 
